00da3a20930dceb657e5a31ee428458daa02c7fa
Minhyeok Park <pmh_only@pmh.codes>
2023-02-01 01:27:33 +0900

    init: initialization

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['standard-with-typescript', 'plugin:prettier/recommended'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  rules: {},
+}
